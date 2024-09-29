f0e5f9172e39e2eb7afd758e1ff8aaa004ea69dd
Andrew Madea <AndrewMadea@gmail.com>
2024-09-28 23:45:42 -0400

    JS file to update the date

---
+++ dashboard/static/js/dateTime.js
@@ -0,0 +1,21 @@
+$(document).ready( function () {
+    updateTime();
+
+    setInterval(updateTime(),1000)
+});
+
+
+function updateTime() {
+    
+    let dt= new Date();
+    $("#datetime").empty();
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    };
+
+    $("#datetime").append(dt.toLocaleDateString("en-US", options));
+}
+
