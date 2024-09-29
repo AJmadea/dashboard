6faddd344b46efaa53c6274af40647f22f3ccad7
Andrew Madea <AndrewMadea@gmail.com>
2024-09-29 10:48:38 -0400

    Removed logs

--- dashboard/static/js/NJT.js
+++ dashboard/static/js/NJT.js
@@ -2,11 +2,11 @@ $(document).ready( function () {
     updateNJT();
 });
 
-function create_card(bus, description) {
-    return `<div class="card text-light bg-dark m-10">
+function create_card(bus, description,title) {
+    return `<div class="card text-light bg-dark pt-5">
         <div class="card-body">
             <h5 class="card-title">${bus}</h5>
-            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <h6 class="card-subtitle mb-2 text-muted">${title}</h6>
             <p class="card-text">${description}</p>
             
         </div>
@@ -26,11 +26,23 @@ function updateNJT() {
         // Function to call when to
         // request is ok 
         success: function (data) {
-            console.log(data);
+            
             $("#NJT").empty();
+
+            if (data.length == 0) {
+                $("#NJT").hide();
+
+                /*$("#weatherCol").addClass("col");
+                $("#weatherCol").removeClass("col-8");*/
+            } else { 
+                $("#NJT").show();
+                /*$("#weatherCol").removeClass("col");
+                $("#weatherCol").addClass("col-8");*/
+            }
+
             for (let i=0; i<data.length;i++) {
-                let card= create_card(data[i]["Bus"], data[i]["Description"]);
-                console.log(card);
+                let card= create_card(data[i]["Bus"], data[i]["Description"],data[i]["Title"]);
+                
                 $("#NJT").append(card);
             }
             
