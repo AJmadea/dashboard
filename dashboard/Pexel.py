503c1c7a8b16cca133e7145e9d19b1accab81cb1
Andrew Madea <AndrewMadea@gmail.com>
2024-11-03 16:21:43 -0500

    uses files

--- dashboard/Pexel.py
+++ dashboard/Pexel.py
@@ -1,29 +1,44 @@
 import PIL.Image
 import requests,json
 import os
-from datetime import datetime
+from datetime import datetime, date
+import pandas as pd
 
 import PIL
 
-season_map = {
-    12:"winter", 1:"winter", 2:"winter",
-    3:"spring", 4:"spring", 5:"spring",
-    6:"summer", 7:"summer", 8:"summer",
-    9:"autumn", 10:"autumn", 11:"autumn",
+specificDates = pd.read_excel('dashboard/static/specificDates.xlsx')
+specificDates.set_index(['Month','Day'], inplace=True)
 
-}
+season_map = pd.read_excel('dashboard/static/monthQueries.xlsx')
+season_map.set_index('Month', inplace=True)
+
 
 def get_seasonal_image():
-
     PEXEL_API_KEY = os.getenv('PEXEL_API_KEY')
 
-    headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
         "Authorization": PEXEL_API_KEY
     }
 
     dt=datetime.now().date()
-    season = season_map[dt.month]
-    res=requests.get(f"https://api.pexels.com/v1/search?query={season} scenery {dt.strftime('%B')}&per_page=32&orientation=landscape&size=large", headers=headers)
+
+    try:
+        query = specificDates.loc[(dt.month, dt.day), 'Query']
+    except:
+        query = None
+
+
+    try:
+        season = season_map.loc[dt.month, 'Query']
+    except: 
+        season= None
+
+
+    query = query or season
+    #{dt.strftime('%B')}
+    print(query)
+    res=requests.get(f"https://api.pexels.com/v1/search?query={query}&per_page=32&orientation=landscape&size=large", headers=headers)
     res.close()
     
     return res.json()['photos'][dt.day]
@@ -34,7 +49,6 @@ def get_image():
 
     headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
         "Authorization": PEXEL_API_KEY
-
     }
 
     dt=datetime.now().date()
