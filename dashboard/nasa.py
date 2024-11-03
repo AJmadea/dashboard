10046b6bd44fa0fe3118ba3bd63f9bdb81d3e551
Andrew Madea <AndrewMadea@gmail.com>
2024-11-03 15:46:31 -0500

    Doesn't only get the hd url.  Returns all json

--- dashboard/nasa.py
+++ dashboard/nasa.py
@@ -5,9 +5,9 @@ import os
 def get_apod():
     NASA_API_KEY= os.getenv("NASA_API_KEY")
 
-    res=requests.get("https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}")
+    res=requests.get(f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}")
     res.close()
     jData=res.json()
 
-    hdUrl=jData['hdurl']
-    return hdUrl
+    #hdUrl=jData.get('hdurl','')
+    return jData
