0b763446fc631e1fb8021ad220f1dca90db80e45
Andrew Madea <AndrewMadea@gmail.com>
2024-11-12 20:01:17 -0500

    3 hour difference

--- dashboard/static/js/dateTime.js
+++ dashboard/static/js/dateTime.js
@@ -8,9 +8,7 @@ $(document).ready( function () {
 
 function weatherWidget() {
     let widget = `<a class="weatherwidget-io" href="https://forecast7.com/en/40d90n74d06/maywood/?unit=us" data-label_1="Maywood" data-label_2="Weather" data-font="Times New Roman" data-icons="Climacons Animated" data-theme="original" >Maywood Weather</a>
-    <script>
-        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-    </script>
+    
     `;
 
     $("#weatherwidget").empty();
@@ -20,11 +18,12 @@ function weatherWidget() {
 function updateTime() {
 
     let current= new Date();
-    if ((date.getTime() - current.getTime() >= 8) || date.getDay()!=current.getDay()) {
+    if ((date.getTime() - current.getTime() >= 3) || date.getDay()!=current.getDay()) {
         location.reload();
     }
 
-    weatherWidget();
+    //weatherWidget();
+
     let dt= new Date();
     $("#datetime").empty();
     const options = {
